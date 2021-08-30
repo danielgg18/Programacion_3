73b2601c7627257d2661f20937de4fa194363a0e
Edgar Daniel García García <danielgg1103@gmail.com>
2021-08-30 13:55:07 -0500

    Inicio del programa

---
+++ autoevalucaion.c
@@ -0,0 +1,12 @@
+/*
+	Name: autoevaluacion.c
+	Copyright: 
+	Author: Daniel García García
+	Date: 30/08/21 
+	Description: Programa de un arreglo dinamico de estructuras
+		
+    */
+
+//Directivas del preprocesador
+#include <stdio.h>
+#include <stdlib.h>		//Incluye la funcion malloc

930e3d975229b4c681214f0b7b32fe5097b37e56
Edgar Daniel García García <danielgg1103@gmail.com>
2021-09-08 12:14:27 -0500

    funcion pila vacia

--- arregloPila.c
+++ arregloPila.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define VERDADERO 1
+#define FALSO 0
+
 
 typedef struct {
     char sNombre[20];
     int iValor;
 } nNodo;
 
+/*
+	Name: pila_vacia
+	Copyright: 
+	Author: Daniel Garcia
+	Date: 26/08/21 
+	Description: Funcion que verifica si la pila esta vacia
+        Parametros: Ninguno
+		Regresa:
+            VERDADERO si esta vacia la pila  
+            FALSO si no esta vacia
+*/
 int pila_vacia () {
 
-    /*Cuando la pila esta vacia*/    
-    
+    if(tope - inicio == iTamanio_pila)
+        return VERDADERO;
+    else
+        return FALSO;
 }
 
-
+/*
+	Name: pila_llena
+	Copyright: 
+	Author: Daniel Garcia
+	Date: 26/08/21 
+	Description: Funcion que verifica si la pila esta llena
+        Parametros: Ninguno
+		Regresa:
+            VERDADERO si esta vacia la pila  
+            FALSO si no esta vacia
+*/
 int pila_llena () {
 
     /*Cuando la pila esta llena*/    

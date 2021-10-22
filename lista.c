da07f38caa74e03ef901e577e49b68001894c721
Edgar Daniel García García <danielgg1103@gmail.com>
2021-10-22 00:21:05 -0500

    Update lista.c
    funciones creaInicio y creaFinal

--- lista.c
+++ lista.c
@@ -25,16 +25,36 @@ void creaInicio(pNodo *liga, int numCanciones, char album[22], char nomArtista[2
 	
 	do{
 		Q = (pNodo)malloc(sizeof(tipoNodo));
-		Q = (pNodo)malloc(sizeof(tipoNodo));
 		Q->canciones = numCanciones;
-		strcpy(P->titulo, album);
-		strcpy(P->artista, nomArtista);
+		strcpy(Q->titulo, album);
+		strcpy(Q->artista, nomArtista);
 		Q->liga = P;
 		P = Q;
 	} while (Q == NULL);
 	
 }
 
+void creaFinal(pNodo *liga, int numCanciones, char album[22], char nomArtista[20]){
+	pNodo P, Q, T;
+
+	P = (pNodo)malloc(sizeof(tipoNodo));
+	P->canciones = numCanciones;
+	strcpy(P->titulo, album);
+	strcpy(P->artista, nomArtista);
+	P->liga = NULL;
+	T = P;
+	
+	do{
+		Q = (pNodo)malloc(sizeof(tipoNodo));
+		Q->canciones = numCanciones;
+		strcpy(Q->titulo, album);
+		strcpy(Q->artista, nomArtista);
+		Q->liga = NULL;
+		T->liga= Q;
+		T = Q;
+	} while (Q == NULL);
+	
+}
 
 int main(){
 	pNodo inicio = NULL, 
@@ -48,12 +68,12 @@ int main(){
 		fflush(stdin);
 		//system("cls");
 		printf("\t*******\n\t Lista ligada\n\t*******\n");
-		printf("1. Crear lista desde el inicio\n2. Eliminar\n3. Ver Extremos\n4. Ver Cola\n5. Salir\n");
+		printf("1. Crear lista desde el inicio\n2. Crear lista desde el final\n3. Ver Extremos\n4. Ver Cola\n5. Salir\n");
 		printf("Selecione una opci%cn: ", 162);
 		scanf("%d", &eOpcion);
 
 		switch(eOpcion){
-			case 1: printf("Ingrese el nuevo elemento: \n");
+			case 1: printf("Ingrese el numero de canciones del album: \n");
 					scanf("%d", &numCanciones);
                     printf("Ingresa el nombre del album: \n");
                     fflush(stdin);
@@ -64,9 +84,15 @@ int main(){
 					creaInicio(&inicio, numCanciones, album, nomArtista);
 					break;
 
-			case 2: eNumero = deCola(&inicio, &final);
-					if (eNumero!= ERROR_VALOR)
-						printf("Se eliminaron los elementos %d-%s de la cola\n", eNumero, sNombre);
+			case 2: printf("Ingrese el numero de canciones del album: \n");
+					scanf("%d", &numCanciones);
+                    printf("Ingresa el nombre del album: \n");
+                    fflush(stdin);
+                    gets(album);
+					printf("Ingresa el nombre del artista: \n");
+                    fflush(stdin);
+					gets(nomArtista);
+					creaFinal(&inicio, numCanciones, album, nomArtista);
 					break;
 
 			case 3: ver_extremos(inicio, final);

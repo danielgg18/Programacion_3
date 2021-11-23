c5279038cedf6c7c94f06c65519d0cb90abd96fe
Edgar Daniel García García <danielgg1103@gmail.com>
2021-11-23 15:52:53 -0600

    Update proyectoFinal.c
    se cambio de orden el strcpy

--- proyectoFinal.c
+++ proyectoFinal.c
@@ -96,7 +96,7 @@ void leer_registros (pNodo *P) {
  
 	    if (fread (&nodo, tamanio, 1, arch) != 0 ){
 	    	    *P = (pNodo) malloc (sizeof(tipoNodo));
-	    		gets(nodo.NomAlbum);
+	    		strcpy(nodo.NomAlbum, (*P)->NomAlbum);
 				fflush(stdin);
 				strcpy((*P)->NomArtista, nodo.NomArtista);
 				(*P)->NumCanciones=nodo.NumCanciones;
